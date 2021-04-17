e14b2ea453e1e81c025a807013f27b372fca4be2
royster <rcantu786@gmail.com>
2021-04-17 13:35:39 -0500

    added answer check display

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,10 +1,9 @@
 var startButton = document.querySelector('#start-btn');
 var answerButton = document.querySelector('#card-btn')
 var questionCard = document.querySelector('#card');
-const wrapper = document.getElementById('card'); 
-const footer = document.querySelector('footer');
+//var wrapper = document.querySelector('#wrapper');
 var slide = 0;
-var isOver = false;
+var anstext = '';
 
 var quiz = [
     {
@@ -46,6 +45,8 @@ var quiz = [
 
 
 var eventHandler = function(event) {
+    console.log("...Listening...");
+    console.log("Slide is - " + slide);
     const isButton = event.target.nodeName === 'BUTTON';
     switch(event.target.id) {
         
@@ -54,13 +55,13 @@ var eventHandler = function(event) {
             break;
 
         case 'high-scores':
-            Show();
+            console.log("clicked high-scores");
             break;
 
         case 'a':
         case 'b':
         case 'c':
-        case 'd': 
+        case 'd':
             checkAnswer(slide, event.target.id);
             checkState();
             break;
@@ -70,24 +71,31 @@ var eventHandler = function(event) {
     }
 } 
 
-function Show(){
-    alert("lsjdf");
-};
-    
+function checkState(){
+    console.log("...In checkState...");
+    if (slide < quiz.length) {
+        displayQuestion(slide);
+    } else if (slide === quiz.length) {
+        endGame();
+    }
+}
+
 function displayQuestion(slide) {
-        btn1 = '<button id="a" class="card-button" type="button">' + quiz[slide].a + '</button>'
-        btn2 = '<button id="b" class="card-button" type="button">' + quiz[slide].b + '</button>'
-        btn3 = '<button id="c" class="card-button" type="button">' + quiz[slide].c + '</button>'
-        btn4 = '<button id="d" class="card-button" type="button">' + quiz[slide].d + '</button>'
-        questionCard.innerHTML = '<div><h1>' + quiz[slide].q + '</h1></div><div id="buttons">' + btn1 + btn2 + btn3 + btn4 + "</div";
+        console.log("...displayQuestion...");
+        btn1 = '<button id="a" class="button" type="button">' + quiz[slide].a + '</button>'
+        btn2 = '<button id="b" class="button" type="button">' + quiz[slide].b + '</button>'
+        btn3 = '<button id="c" class="button" type="button">' + quiz[slide].c + '</button>'
+        btn4 = '<button id="d" class="button" type="button">' + quiz[slide].d + '</button>'
+        questionCard.innerHTML = '<div><h1>' + quiz[slide].q + '</h1></div><div id="buttons">' + btn1 + btn2 + btn3 + btn4 + '</div><footer id="footer">' + anstext + '</footer>';
 };
 
 function checkAnswer(qNumber, qResponse) {
+    console.log("...In checkAnswer...");
     qAnswer = quiz[qNumber].ans;
     if (qResponse === qAnswer) {
-        footer.innerHTML = "<div><h3>Correct</h3></div>";
+        anstext = 'Correct!';
     } else {
-        footer.innerHTML = "<div><h3>Wrong</h3></div>";
+        anstext = 'Wrong!'
         // decrease time here
     }
     slide++
@@ -102,8 +110,7 @@ function checkState(){
 }
 
 function endGame() {
-    questionCard.innerHTML = '<div><h1>GAME OVER</h1>';
+    questionCard.innerHTML = '<h1>GAME OVER</h1>';
 }
 
 wrapper.addEventListener('click', eventHandler);
-
