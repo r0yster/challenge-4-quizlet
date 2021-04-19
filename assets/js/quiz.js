2071459f534157ef9dd0f2c84ab656227ba0c95f
royster <rcantu786@gmail.com>
2021-04-18 23:54:16 -0500

    completed local storage

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,60 +1,65 @@
+// JQuery Shorthand
 const questionCard = document.querySelector('#card');
 const header = document.getElementById('')
 const timer =  document.getElementById('timer');
-var slide = 0;
-var timeLeft = 75;
-var anstext = '';
-var stopTimer = false;
+
+// Global Variables
+var slide = 0;              // track where user is in quiz
+var timeLeft = 75;          // default timer value
+var anstext = '';           // variable to display if answer is right or wrong
+var stopTimer = false;      // value to stop timer in certain screens
+var scoresLoaded = false;   // value to load scores once
+var playerScores = [];      // local storage array
 // quiz questions array
 var quiz = [
     {
-        q:'hey this is question 1 here who are tom petty and the heartbreakers', 
-        a:'answer 1',
-        b:'answer 2',
-        c:'answer 3',
-        d:'answer 4',
-        ans: "c"
+        q:'Commonly used data types DO NOT include :', 
+        a:'strings',
+        b:'booleans',
+        c:'alerts',
+        d:'numbers',
+        ans: 'c'
     },{
-        q:'Grasslands, deserts, and prairies are examples of animal ________', 
-        a:'Adaptations',
-        b:'Seasons',
-        c:'Habitats',
-        d:'Organisms',
-        ans: "c"
+        q:'Grasslands, deserts, and prairies are examples of animal __________.', 
+        a:'adaptations',
+        b:'seasons',
+        c:'habitats',
+        d:'organisms',
+        ans: 'c'
     },{
-        q:'hey this is question 3', 
-        a:'answer 1',
-        b:'answer 2',
-        c:'answer 3',
-        d:'answer 4',
-        ans: "d"
+        q:'Arrays in JavaScript can be used to store __________.', 
+        a:'numbers and strings',
+        b:'other arrays',
+        c:'booleans',
+        d:'all of the above',
+        ans: 'd'
     },{   
-        q:'hey this is question 4', 
-        a:'answer 1',
-        b:'answer 2',
-        c:'answer 3',
-        d:'answer 4',
-        ans: "d" 
+        q:'A very useful tool used during development and debugging for printing content to the debugger is:', 
+        a:'JavaScript',
+        b:'terminal/bash',
+        c:'for loops',
+        d:'console.log',
+        ans: 'd' 
     },{    
-        q:'hey this is question 5', 
-        a:'answer 1',
-        b:'answer 2',
-        c:'answer 3',
-        d:'answer 4',
-        ans: "b"
+        q:'String values must be enclosed within __________ when being assigned to variables.', 
+        a:'commas',
+        b:'curly brackets',
+        c:'quotes',
+        d:'parenthesis',
+        ans: 'd'
     }]
 //
 // QUIZ FLOW FUNCTIONS 
 //
 // event handler checking if user click is a button with switch cases on the id of the button
 var eventHandler = function(event) {
-    console.log("slide is " + slide);
+    console.log("scores loaded " + scoresLoaded);
     const isButton = event.target.nodeName === 'BUTTON';
     switch(event.target.id) {
         
         case 'start-btn':
             stopTimer = false;
-            countdown();
+            startTimer();
             checkState();
 
         case 'goto-start':
@@ -70,13 +75,42 @@ var eventHandler = function(event) {
             break;
 
         case 'high-scores':
-        case 'save-name':
             showStats();
+            break;
+
+        case 'save-name':
+            debugger;
+            saveScore();
+            showStats();
+            break;
+
+        case 'clear-scores':
+            clearScores();
+            alert("scored cleared");
             break;
 
         default:
             break;
     }
+}
+
+// start timer function
+function startTimer() {
+    var timeInterval = setInterval(function() {   
+        if (timeLeft > 0) {
+            timer.textContent='Timer : ' + timeLeft + ' seconds';
+            timeLeft--; 
+        } else {
+            timeLeft = 0;
+            timer.textContent='';
+            clearInterval(timeInterval);
+            endQuiz();
+        }
+        if (stopTimer === true) {
+            clearInterval(timeInterval);
+            timer.textContent='';
+        }
+    }, 1000);
 }
 
 // function to check what screen is being shown depenedent on user advancement in quiz
@@ -126,12 +160,13 @@ function endQuiz() {
     '<div id="card" class="end-card">' + 
         '<h1>GAME OVER!</h1>' +
         '<p>Your score was ' + timeLeft +
-        '<p>Please Enter Your Initials</p>' +
-        '<input type="text" name="player-name" placeholder="LT" />' +
+        '<p>Please Enter Your Initials</p>' + 
+        '<input id="player-initials" type="text" placeholder="LT" />' +
         '<button id="save-name">save</button>' +
     '</div>';
 }
 
+// fucntion to display screen start, resets all global variables
 function displayStart() {
     slide=0;
     timeLeft = 75;
@@ -144,36 +179,18 @@ function displayStart() {
         '</p>' +
         '<button id="start-btn" class="button" type="button">Start</button>' 
 }
-//
-// END QUIZ FLOW FUNCTIONS
-//
 
-// TIMER FUNCTIONS
-function countdown() {
-    var timeInterval = setInterval(function() {   
-        if (timeLeft > 0) {
-            timer.textContent='Timer : ' + timeLeft + ' seconds';
-            timeLeft--; 
-        } else {
-            timeLeft = 0;
-            timer.textContent='';
-            clearInterval(timeInterval);
-            endQuiz();
-        }
-        if (stopTimer === true) {
-            clearInterval(timeInterval);
-            timer.textContent='';
-        }
-    }, 1000);
-}
-
-// STORAGE FUNCTIONS
-
-
+// function to show high scores screen
 function showStats() {
     slide=6;
     stopTimer = true;
-
+    var tableDataEl = "";
+    if (!scoresLoaded) {
+        loadScores();
+    }
+    for (var i = 0; i<playerScores.length; i++) {
+        tableDataEl = tableDataEl + '<tr><td>' + playerScores[i].initials + '</td><td>' + playerScores[i].score + '</td>';
+    };
     questionCard.innerHTML = 
     '<div id="card" class="stat-card">' +
         '<h1>High Score</h1>' +
@@ -181,20 +198,45 @@ function showStats() {
             '<tr>' +
                 '<th>Player Initials</th>' +
                 '<th>Score</th>' +
-            '</tr>' +
-            '<tr>' +
-                '<td>name</td>' +
-                '<td>score</td>' +
-            '</tr>' +
-            '<tr>' +
-            '<td>name</td>' +
-            '<td>score</td>' +
-        '</tr>' +
+                tableDataEl +
         '</table>' +
         '<button id="goto-start" class="button" type="button">Start Page</button>' +
+        '<button id="clear-scores" class="button" type="button">Clear Scores</button>' +
     '</div>'
+
+}
+//
+// END QUIZ FLOW FUNCTIONS
+//
+
+// STORAGE FUNCTIONS
+function saveScore() {
+    var scoreEl = {
+        score: 0,
+        initials: "",
+    }
+    scoreEl.score = timeLeft;
+    scoreEl.initials = document.getElementById("player-initials").value.toUpperCase();
+
+    playerScores.push(scoreEl);
+    localStorage.setItem("localScores", JSON.stringify(playerScores));
 }
 
+function loadScores() {
+    var savedScores = JSON.parse(localStorage.getItem("localScores"));
+    if (!savedScores) {
+        return false;
+    }
+    for (var i=0; i<savedScores.length; i++){
+        playerScores.push(savedScores[i]);
+    }
+    scoresLoaded = true;
+}
+
+function clearScores() {
+    localStorage.clear();
+    playerScores = [];
+}
 
 displayStart();
 wrapper.addEventListener('click', eventHandler);
