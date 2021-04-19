13121862fac417fb08aec2455e4c1177c48ae937
royster <rcantu786@gmail.com>
2021-04-18 19:42:22 -0500

    added loop functionality to entire quiz

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,8 +1,10 @@
 const questionCard = document.querySelector('#card');
+const header = document.getElementById('')
 const timer =  document.getElementById('timer');
 var slide = 0;
 var timeLeft = 75;
 var anstext = '';
+var stopTimer = false;
 // quiz questions array
 var quiz = [
     {
@@ -13,12 +15,12 @@ var quiz = [
         d:'answer 4',
         ans: "c"
     },{
-        q:'hey this is question 2', 
-        a:'answer 1',
-        b:'answer 2',
-        c:'answer 3',
-        d:'answer 4',
-        ans: "a"
+        q:'Grasslands, deserts, and prairies are examples of animal ________', 
+        a:'Adaptations',
+        b:'Seasons',
+        c:'Habitats',
+        d:'Organisms',
+        ans: "c"
     },{
         q:'hey this is question 3', 
         a:'answer 1',
@@ -46,30 +48,32 @@ var quiz = [
 //
 // event handler checking if user click is a button with switch cases on the id of the button
 var eventHandler = function(event) {
-    console.log("...Listening...");
-    console.log("Slide is - " + slide);
+    console.log("slide is " + slide);
     const isButton = event.target.nodeName === 'BUTTON';
     switch(event.target.id) {
         
         case 'start-btn':
+            stopTimer = false;
             countdown();
             checkState();
-            break;
 
-        case 'high-scores':
-            showStats();
+        case 'goto-start':
+            checkState();
             break;
 
         case 'a':
         case 'b':
         case 'c':
         case 'd':
-            checkAnswer(slide, event.target.id);
+            checkAnswer(slide,event.target.id);
             checkState();
             break;
 
+        case 'high-scores':
         case 'save-name':
             showStats();
+            break;
+
         default:
             break;
     }
@@ -77,17 +81,25 @@ var eventHandler = function(event) {
 
 // function to check what screen is being shown depenedent on user advancement in quiz
 function checkState(){
-    console.log("...In checkState...");
-    if (slide < quiz.length) {
-        displayQuestion(slide);
-    } else if (slide === quiz.length) {
-        endGame();
+    switch (slide) {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+            displayQuestion(slide);
+            break;
+        case 5:
+            endQuiz();
+            break; 
+        case 6:
+            displayStart();
+            break;            
     }
 }
 
 // function that when given a number, displays the question inside quiz[number]
 function displayQuestion(slide) {
-        console.log("...displayQuestion...");
         btn1 = '<button id="a" class="button" type="button">' + quiz[slide].a + '</button>'
         btn2 = '<button id="b" class="button" type="button">' + quiz[slide].b + '</button>'
         btn3 = '<button id="c" class="button" type="button">' + quiz[slide].c + '</button>'
@@ -97,7 +109,6 @@ function displayQuestion(slide) {
 
 // function that checks users response to question answer
 function checkAnswer(qNumber, qResponse) {
-    console.log("...In checkAnswer...");
     qAnswer = quiz[qNumber].ans;
     if (qResponse === qAnswer) {
         anstext = 'Correct!';
@@ -105,11 +116,12 @@ function checkAnswer(qNumber, qResponse) {
         anstext = 'Wrong!'
         timeLeft -= 10;
     }
-    slide++
+    slide++;
 }
 
 // function to handle end game logic
-function endGame() {
+function endQuiz() {
+    stopTimer = true;
     questionCard.innerHTML = 
     '<div id="card" class="end-card">' + 
         '<h1>GAME OVER!</h1>' +
@@ -119,30 +131,49 @@ function endGame() {
         '<button id="save-name">save</button>' +
     '</div>';
 }
+
+function displayStart() {
+    slide=0;
+    timeLeft = 75;
+    anstext = '';
+    questionCard.innerHTML = 
+        '<h1>Code Quiz Challenge</h1>' +
+        '<p>' +
+            'Try to answer the following code-related quesetions within the time limit.' +
+            'Keep in mind that incorrect answers will penalize your score/time by ten seconds!' +
+        '</p>' +
+        '<button id="start-btn" class="button" type="button">Start</button>' 
+}
 //
 // END QUIZ FLOW FUNCTIONS
 //
 
 // TIMER FUNCTIONS
 function countdown() {
-    var timeInterval = setInterval(function() {
-        if (timeLeft >= 1 && slide != quiz.length) {
-            timer.textContent = 'Timer : ' + timeLeft + " seconds";
-            timeLeft--;
-        } else if (timeLeft === 1) {
-            timer.textContent = 'Timer : ' + timeLeft + " second";
+    var timeInterval = setInterval(function() {   
+        if (timeLeft > 0) {
+            timer.textContent='Timer : ' + timeLeft + ' seconds';
+            timeLeft--; 
         } else {
-            timer.remove();
+            timeLeft = 0;
+            timer.textContent='';
             clearInterval(timeInterval);
-            endGame();
+            endQuiz();
         }
-      }, 1000);
+        if (stopTimer === true) {
+            clearInterval(timeInterval);
+            timer.textContent='';
+        }
+    }, 1000);
 }
 
 // STORAGE FUNCTIONS
 
 
 function showStats() {
+    slide=6;
+    stopTimer = true;
+
     questionCard.innerHTML = 
     '<div id="card" class="stat-card">' +
         '<h1>High Score</h1>' +
@@ -151,8 +182,19 @@ function showStats() {
                 '<th>Player Initials</th>' +
                 '<th>Score</th>' +
             '</tr>' +
+            '<tr>' +
+                '<td>name</td>' +
+                '<td>score</td>' +
+            '</tr>' +
+            '<tr>' +
+            '<td>name</td>' +
+            '<td>score</td>' +
+        '</tr>' +
         '</table>' +
+        '<button id="goto-start" class="button" type="button">Start Page</button>' +
     '</div>'
 }
 
+
+displayStart();
 wrapper.addEventListener('click', eventHandler);
